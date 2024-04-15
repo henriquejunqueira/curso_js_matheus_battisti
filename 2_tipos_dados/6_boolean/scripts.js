5b7184f4bed190ee12f49da1239d87bf8ee1867c
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-04-15 12:59:30 -0300

    subindo novos arquivos

---
+++ 2_tipos_dados/6_boolean/scripts.js
@@ -0,0 +1,11 @@
+// Boolean
+// console.log(true);
+// console.log(false);
+// console.log(10 > 2);
+// console.log(5 > 200);
+
+// Verificando tipo
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof (10 > 2));
+console.log(typeof (5 > 200));
