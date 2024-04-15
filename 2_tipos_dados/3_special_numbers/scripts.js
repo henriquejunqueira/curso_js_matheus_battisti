f3172ef24801681acd5322c0148f1a5f6204a15c
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-04-15 11:42:05 -0300

    subindo aula 4

---
+++ 2_tipos_dados/3_special_numbers/scripts.js
@@ -0,0 +1,9 @@
+// Special Numbers (números que não são números)
+// console.log(Infinity);
+// console.log(-Infinity);
+// console.log(NaN);
+
+// Verificando tipo
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
---
+++ 2_tipos_dados/4_string/scripts.js
@@ -0,0 +1,13 @@
+// Strings
+// console.log("Oi, meu nome é Henrique");
+// console.log('Eu queria comprar um carro');
+// console.log(`Teste`);
+// console.log("Infinity");
+// console.log(Infinity);
+
+// Verificando tipo
+console.log(typeof "Oi, meu nome é Henrique");
+console.log(typeof 'Eu queria comprar um carro');
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
